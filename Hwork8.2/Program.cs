bfbf45c2531bdc33ed8b6b930f105559d0239611
Наталья Фёдорова <natalyafedorova5@gmail.com>
2021-10-28 00:14:20 +0300

    Добавьте файлы проекта.

---
+++ Hwork8.2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Hwork8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Программно создайте текстовый файл и запишите в него 10 случайных чисел. Затем программно откройте созданный файл, рассчитайте сумму чисел в нем, ответ выведите на консоль.
+            string path = @"D:\!ОБУЧЕНИЕ\Проекты\Задание 8. Работа с файловой системой\Hwork8\Hwork8.2\CaseText.txt";
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+            Random random = new Random();
+            StreamWriter numbers = new StreamWriter(path, false);
+            for (int i = 0; i < 10; i++)
+            {
+                int n = random.Next(-5, 5);// для себя задал параметры
+                numbers.Write($"{n} ");
+            }
+            numbers.Close();
+            StreamReader Num = new StreamReader(path);
+            string S = Num.ReadLine();
+            S = S.Trim();
+            Console.WriteLine($"Числа:\n{S}");
+            string[] P = S.Split(' ');
+            int Sum = 0;
+            for (int i = 0; i < P.Length; i++)
+            {
+                Sum += Convert.ToInt32(P[i]);
+            }
+            Console.WriteLine($"Сумма чисел:\n{Sum}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Hwork8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Hwork8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\!ОБУЧЕНИЕ\Проекты\Задание 8. Работа с файловой системой\Hwork8";
+            if (Directory.Exists(path))
+            {
+                string[] subdirectories = Directory.GetDirectories(path);
+                foreach (string s in subdirectories)
+                {
+                    Console.WriteLine("Подкаталоги");
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+                string[] files = Directory.GetFiles(path);
+                foreach (string p in files)
+                {
+                    Console.WriteLine("Файлы");
+                    Console.WriteLine(p);
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
